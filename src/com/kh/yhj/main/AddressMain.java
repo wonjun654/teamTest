484b1385d98772260455c316eecfbd7bca9d119e
yhj <yhj@192.168.0.25>
2019-08-12 00:41:04 +0900

    윤혁준 주말 간 문제제출

---
+++ src/com/kh/yhj/main/AddressMain.java
@@ -0,0 +1,11 @@
+package com.kh.yhj.main;
+
+import com.kh.yhj.example.Example;
+
+public class AddressMain {
+	public static void main(String[] args) {
+		Example example = new Example();
+		example.input();
+		example.output();
+	}
+}
